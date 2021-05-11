11a750300a1b1ff0c1753b0235f9d3cd1168f403
Danita Forbes <danitacodes@gmail.com>
2021-05-10 21:20:04 -0700

    Finished calculator app

--- calculator.js
+++ calculator.js
@@ -1,9 +1,33 @@
 const express = require('express');
 
-const app = express();
 
-app.get('/', function(request, response){
-    response.send('Hello');
+const app = express();
+app.use(express.urlencoded({extended: true}))
+
+app.get("/", function(request, response){
+    response.sendFile(__dirname + "/index.html");
+})
+
+app.post("/", function(request, response){
+var num1 = Number(request.body.num1);
+var num2 = Number(request.body.num2);
+
+var result = num1 + num2;
+
+    response.send("The result is " + result);
+})
+
+app.get("/bmicalculator", function(request, response){
+    response.sendFile(__dirname + "/bmicalculator.html");
+})
+
+app.post("/bmicalculator", function(request, response){
+    var weight = parseFloat(request.body.weight);
+    var height = parseFloat(request.body.height);
+
+    var bmi = weight / (height * height);
+
+    response.send("Your BMI is " + bmi);
 })
 
 app.listen(3000, function(){
